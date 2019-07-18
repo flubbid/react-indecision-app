2fb3ce9dadcfac781a0b578c9d78ec28cc4ed3b9
flubbid <brendanwilson1@gmail.com>
2019-07-18 15:00:00 -0500

    fixes error thrown from no div on header class

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 class Header extends React.Component {
     render(){
         return (
+            <div>
             <h1>Indecision</h1>
             <h2>Put your life in the hands of a computer</h2>
+            </div>
         )
     }
 }
