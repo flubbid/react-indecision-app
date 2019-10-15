a29a93aa6cbb5d3382efda4b4756d0be915bad67
flubbid <brendanwilson1@gmail.com>
2019-10-15 12:32:07 -0500

    adds error message when typing in same information

--- src/app.js
+++ src/app.js
@@ -111,19 +111,25 @@ class AddOption extends React.Component{
     constructor(props){
         super(props);
         this.handleAddOption = this.handleAddOption.bind(this)
+        this.state = {
+            error: undefined
+        };
     }
         
     handleAddOption(e){
        e.preventDefault()
        const option = e.target.elements.option.value.trim();
-        if(option){
-            this.props.handleAddOption(option)
-        }
+       const error = this.props.handleAddOption(option)
+
+       this.setState(()=>{
+            return { error }
+       })
     }
     
     render(){
         return(
             <div>
+            {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}> 
                 <input type="text" name="option">
                 
